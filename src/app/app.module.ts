7b8ccae53364dbb7ddbd5e1d8c265b6ade06ce78
iconmedios <jean@iconmedios.com>
2019-06-12 14:10:22 -0500

    Creación de componente pages.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,41 +2,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 // Rutas 
 import { APP_ROUTES } from './app.routes';
-
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { NopagefoundComponent } from './shared/breadcrumbs/nopagefound/nopagefound.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { Graficas1Component } from './pages/graficas1/graficas1.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { FooterComponent } from './shared/footer/footer.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { PagesComponent } from './pages/pages.component';
+// import { NopagefoundComponent } from './shared/breadcrumbs/nopagefound/nopagefound.component';
+
 import { RegisterComponent } from './login/register.component';
 
-
+// Modulos
+import { PagesModule } from './pages/pages.module';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    NopagefoundComponent,
-    DashboardComponent,
-    Graficas1Component,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    FooterComponent,
-    ProgressComponent,
-    PagesComponent,
-    RegisterComponent
+    RegisterComponent,
+   
   ],
   imports: [
     BrowserModule,
     APP_ROUTES,
+    PagesModule
 
   ],
   providers: [],
